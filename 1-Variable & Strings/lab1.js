6f69940ea5008db5d8d4790ea5dfb4cc3c17794e
KebbaPhilip <kebbaphilip@gmail.com>
2026-05-23 15:39:38 +0000

    Js variables

---
+++ 1-Variable & Strings/lab1.js
@@ -0,0 +1,28 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Lavida";
+let botLocation = "Mars";
+let favoriteLanguage = "Pyhon";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  favoriteLanguage +
+  " is an excutable pseudocode, ooh it is  too easy to read!";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage +
+  " is not aggressive like a real snake, it is in fact not poisonous";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " It is named after a British comedy show";
+
+console.log(codingFact);
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
---
+++ 1-Variable & Strings/lab2.js
@@ -0,0 +1,56 @@
+let adjective, noun, verb, place, adjective2, noun2;
+
+adjective = "huge";
+noun = "Elephant";
+verb = "angry";
+place = "valley";
+adjective2 = "big";
+noun2 = "apples";
+
+let firstStory;
+
+firstStory =
+  "Once upon a time, there was a(n) " +
+  [adjective] +
+  " " +
+  [noun] +
+  " who loved to eat " +
+  [noun2] +
+  ". The " +
+  [noun] +
+  " lived in a " +
+  [place] +
+  " and had " +
+  [adjective2] +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "small";
+noun = "Rat";
+verb = "naughty";
+place = "cupboard";
+adjective2 = "tiny";
+noun2 = "berries";
+
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  [adjective] +
+  " " +
+  [noun] +
+  " who loved to eat " +
+  [noun2] +
+  ". The " +
+  [noun] +
+  " lived in a " +
+  [place] +
+  " and had " +
+  [adjective2] +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("Second story: " + secondStory);
+
